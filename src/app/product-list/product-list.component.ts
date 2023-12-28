d303b052b959cd6cf840e02ec330160f0efb1008
AdaCra <ahannath@gmail.com>
2023-12-28 15:11:11 +0100

    restart

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  // name = "John Doe"
+  addToCart: number = 0;
+  product = {
+    name: 'iPhone X',
+    price: 999,
+    color: 'Black',
+    discount: 5,
+    inStock: 10,
+    pImage: '/assets/images/iphone.png',
+  };
+
+  getDiscountedPrice() {
+    return (
+      this.product.price - (this.product.price * this.product.discount) / 100
+    );
+  }
+
+  onNameChange() {
+    //   this.name=event.target.value
+  }
+  decrementCartValue() {
+    if (this.addToCart > 0) {
+      this.addToCart--;
+    }
+  }
+  incrementCartValue() {
+    if (this.addToCart < this.product.inStock) {
+      this.addToCart++;
+    }
+  }
+}
